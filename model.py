df5ef4bd60e901f5ac95d386700877863f973e52
Yuki Falcon <yfalcon8@gmail.com>
2016-08-12 00:02:24 +0000

    Working on basic routes in Flask app.

--- model.py
+++ model.py
@@ -12,6 +12,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 # This set up allows my app the ability to talk to SQLite, PostgreSQL, MySQL
 # and more.
 
@@ -22,8 +23,6 @@ def connect_to_db(app):
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///contacts'
     db.app = app
     db.init_app(app)
-
-connect_to_db(app)
 
 ##############################################################################
 # Create my ORM. Allows for object-orientation into SQL.
@@ -51,8 +50,7 @@ class User(db.Model):
     # are allowed. The (30) indicates that this field will fit a max of
     # 30 characters.
     first_name = db.Column(db.String(30),
-                          nullable=False)
-
+                           nullable=False)
 
     # The nullability of this field is set to False, as I want to require my
     # users to input their name.
@@ -68,33 +66,457 @@ class User(db.Model):
     password = db.Column(db.String(20),
                          nullable=False)
 
-    bday = db.Column(db.Date(20),
+    bday = db.Column(db.Date,
+                     nullable=False)
+
+
+class Recommendation(db.Model):
+    """Stores information about my users."""
+
+    __tablename__ = "recommendations"
+
+    rcmdn_id = db.Column(db.Integer,
+                         primary_key=True,
+                         autoincrement=True)
+
+    # The default is for those users who do not feel like specifying the type
+    # relationship they have with the contact they just imported. There will
+    # be a default set of tips on how to reach out.
+    relatp_code = db.Column(db.Integer,
+                            db.ForeignKey('relationships.relatp_code'),
+                            default=100)
+
+    rcmdn_text = db.Column(db.Text,
+                           nullable=False,
+                           unique=True)
+
+    # Join the recommendation table and relationship table through the
+    # relatp_code. This allows me to navigate from the a user's contact to his/her
+    # associated recommendations and vice versa.
+    relatp = db.relationship("Relationship", backref=db.backref("rcmdn"))
+
+
+class Event(db.Model):
+    """Stores information about each event."""
+
+    __tablename__ = "events"
+
+    event_id = db.Column(db.Integer,
+                         autoincrement=True,
+                         primary_key=True)
+
+    user_id = db.Column(db.Integer,
+                        db.ForeignKey('users.user_id'))
+
+    scheduled_at = db.Column(db.DateTime,
+                             nullable=False)
+
+    relatp_id = db.Column(db.Integer)
+
+    rcmdn_text = db.Column(db.Text,
+                           nullable=False)
+
+    user = db.relationship('User', backref='event')
+
+
+class Relationship(db.Model):
+    """Stores all of a users' contacts info."""
+
+    __tablename__ = "relationships"
+
+    relatp_id = db.Column(db.Integer,
+                          autoincrement=True,
+                          primary_key=True)
+
+    relatp_code = db.Column(db.Integer,
+                            nullable=False,
+                            default=100)
+
+    user_id = db.Column(db.Integer,
+                        db.ForeignKey('users.user_id'))
+
+    email = db.Column(db.String(50))
+
+    bday = db.Column(db.Date)
+
+    phone = db.Column(db.String(15))
+
+    work = db.Column(db.String(50))
+
+    edu = db.Column(db.String(50))
+
+    fb = db.Column(db.String(50))
+
+    linked_in = db.Column(db.String(50))
+
+    twitter = db.Column(db.String(50))
+
+    google_plus = db.Column(db.String(50))
+
+    meet_up = db.Column(db.String(50))
+
+    github = db.Column(db.String(50))
+
+    pinterest = db.Column(db.String(50))
+
+    reddit = db.Column(db.String(50))
+
+    word_press = db.Column(db.String(50))
+
+    yelp = db.Column(db.String(50))
+
+    youtube = db.Column(db.String(50))
+
+    skype = db.Column(db.String(50))
+
+    other_social_media = db.Column(db.String(50))
+
+
+    user = db.relationship("User", backref=db.backref("relationships"))
+
+    recommendation = db.relationship("Recommendation",
+                                     secondary='rcmdns_relatps',
+                                     backref=db.backref("relationship"))
+
+
+class RecommendationRelationship(db.Model):
+    """Association table between recommendation and relationship table.
+
+    Describes the method of reaching out that's tied to each contact.
+    """
+
+    __tablename__ = "rcmdns_relatps"
+
+    rcmdnRelatp_id = db.Column(db.Integer,
+                               autoincrement=True,
+                               primary_key=True)
+
+    rcmdn_id = db.Column(db.Integer,
+                         db.ForeignKey('recommendations.rcmdn_id'),
                          nullable=False)
 
-
-# class Recommendation(db.Model):
-#     """Stores information about my users."""
-
-#     # Specifies table name.
-#     __tablename__ = "recommendations"
-
-#     # db.Column creates a column in the users table called user_id. db.Integer
-#     # specifies the type of column. This column sets my primary key to be
-#     # an automatically increasing number.
-#     rcmdn_id = db.Column(db.Integer,
-#                         primary_key=True,
-#                         autoincrement=True)
-
-#     relat_code = db.Column()
+    relatp_code = db.Column(db.Integer,
+                            db.ForeignKey('relationships.relatp_code'),
+                            default=100,
+                            nullable=False)
 
 
+class Gift_Idea(db.Model):
+    """Separately stores each gift idea a user has for his/her contact."""
 
-# def connect_to_db(app):
-#     """Conenct the database to the Flask app."""
+    __tablename__ = "gift_ideas"
 
-#     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///contacts'
-#     db.app = app
-#     db.init_app(app)
+    gift_idea_id = db.Column(db.Integer,
+                             nullable=False,
+                             primary_key=True)
+
+    relatp_id = db.Column(db.Integer,
+                          db.ForeignKey('relationships.relatp_id'))
+
+    gift_idea = db.Column(db.Text)
+
+    # The backref is pluralized as gift_ideas because I'm expecting a list of
+    # gift_ideas back. This is a one-to-many relationship so I'll be getting
+    # one contact back.
+    relatp = db.relationship("Relationship", backref=db.backref("gift_ideas"))
+
+
+class Goal(db.Model):
+    """Separately stores each goal a user has for his/her contact."""
+
+    __tablename__ = "goals"
+
+    goal_id = db.Column(db.Integer,
+                        nullable=False,
+                        primary_key=True)
+
+    relatp_id = db.Column(db.Integer,
+                          db.ForeignKey('relationships.relatp_id'))
+
+    goal = db.Column(db.Text)
+
+    relatp = db.relationship("Relationship", backref=db.backref("goals"))
+
+
+class Note(db.Model):
+    """Separately stores a note a user has for his/her contact.
+
+    For example, 'Paige is planning a vacation to the Bahamas in July.'"""
+
+    __tablename__ = "notes"
+
+    note_id = db.Column(db.Integer,
+                        nullable=False,
+                        primary_key=True)
+
+    relatp_id = db.Column(db.Integer,
+                          db.ForeignKey('relationships.relatp_id'))
+
+    note = db.Column(db.Text)
+
+    relatp = db.relationship("Relationship", backref=db.backref("notes"))
+
+
+class Pet(db.Model):
+    """Separately stores pets a users contact has."""
+
+    __tablename__ = "pets"
+
+    pet_id = db.Column(db.Integer,
+                       nullable=False,
+                       primary_key=True)
+
+    relatp_id = db.Column(db.Integer,
+                          db.ForeignKey('relationships.relatp_id'))
+
+    pet = db.Column(db.Text)
+
+    relatp = db.relationship("Relationship", backref=db.backref("pets"))
+
+
+class Family(db.Model):
+    """Separately stores a family member a user has noted for his/her contact."""
+
+    __tablename__ = "family"
+
+    family_id = db.Column(db.Integer,
+                          nullable=False,
+                          primary_key=True)
+
+    relatp_id = db.Column(db.Integer,
+                          db.ForeignKey('relationships.relatp_id'))
+
+    family = db.Column(db.Text)
+
+    relatp = db.relationship("Relationship", backref=db.backref("family"))
+
+
+class Hobby(db.Model):
+    """Separately stores each gift idea a user has for his/her contact."""
+
+    __tablename__ = "hobbies"
+
+    hobby_id = db.Column(db.Integer,
+                         nullable=False,
+                         primary_key=True)
+
+    relatp_id = db.Column(db.Integer,
+                          db.ForeignKey('relationships.relatp_id'))
+
+    hobby = db.Column(db.Text)
+
+    relatp = db.relationship("Relationship", backref=db.backref("hobbies"))
+
+
+class Like(db.Model):
+    """Separately stores each like that a user has noted for his/her contact."""
+
+    __tablename__ = "likes"
+
+    like_id = db.Column(db.Integer,
+                        nullable=False,
+                        primary_key=True)
+
+    relatp_id = db.Column(db.Integer,
+                          db.ForeignKey('relationships.relatp_id'))
+
+    likes = db.Column(db.Text)
+
+    relatp = db.relationship("Relationship", backref=db.backref("likes"))
+
+
+class Dislike(db.Model):
+    """Separately stores each dislike a user has noted for his/her contact."""
+
+    __tablename__ = "dislikes"
+
+    dislike_id = db.Column(db.Integer,
+                           nullable=False,
+                           primary_key=True)
+
+    relatp_id = db.Column(db.Integer,
+                          db.ForeignKey('relationships.relatp_id'))
+
+    dislike = db.Column(db.Text)
+
+    relatp = db.relationship("Relationship", backref=db.backref("dislikes"))
+
+
+class Pet_Peeve(db.Model):
+    """Separately stores each pet peeve a user has noted for his/her contact."""
+
+    __tablename__ = "pet_peeves"
+
+    pet_peeve_id = db.Column(db.Integer,
+                             nullable=False,
+                             primary_key=True)
+
+    relatp_id = db.Column(db.Integer,
+                          db.ForeignKey('relationships.relatp_id'))
+
+    pet_peeve = db.Column(db.Text)
+
+    relatp = db.relationship("Relationship", backref=db.backref("pet_peeves"))
+
+
+class Favorite_Food(db.Model):
+    """Separately stores each favorite food a user has noted for his/her contact."""
+
+    __tablename__ = "fav_foods"
+
+    fav_food_id = db.Column(db.Integer,
+                            nullable=False,
+                            primary_key=True)
+
+    relatp_id = db.Column(db.Integer,
+                          db.ForeignKey('relationships.relatp_id'))
+
+    fav_food = db.Column(db.Text)
+
+    relatp = db.relationship("Relationship", backref=db.backref("fav_foods"))
+
+
+class Favorite_Drink(db.Model):
+    """Separately stores favorite drink a user has noted for his/her contact."""
+
+    __tablename__ = "fav_drinks"
+
+    fav_drink_id = db.Column(db.Integer,
+                             nullable=False,
+                             primary_key=True)
+
+    relatp_id = db.Column(db.Integer,
+                          db.ForeignKey('relationships.relatp_id'))
+
+    fav_drink = db.Column(db.Text)
+
+    relatp = db.relationship("Relationship", backref=db.backref("fav_drinks"))
+
+
+class Favorite_Restaurant(db.Model):
+    """Separately stores each favorite restaurant a user noted about his/her contact."""
+
+    __tablename__ = "fav_restaurants"
+
+    fav_restaurant_id = db.Column(db.Integer,
+                                  nullable=False,
+                                  primary_key=True)
+
+    relatp_id = db.Column(db.Integer,
+                          db.ForeignKey('relationships.relatp_id'))
+
+    fav_restaurant = db.Column(db.Text)
+
+    relatp = db.relationship("Relationship", backref=db.backref("fav_restaurants"))
+
+
+class Favorite_Sports_Team(db.Model):
+    """Separately stores each favorite sports team a user noted for his/her contact."""
+
+    __tablename__ = "fav_sports_teams"
+
+    fav_sports_team_id = db.Column(db.Integer,
+                                   nullable=False,
+                                   primary_key=True)
+
+    relatp_id = db.Column(db.Integer,
+                          db.ForeignKey('relationships.relatp_id'))
+
+    sports_team = db.Column(db.Text)
+
+    relatp = db.relationship("Relationship", backref=db.backref("fav_sports_teams"))
+
+
+class Favorite_Brand(db.Model):
+    """Separately stores each favorite brand a user has noted for his/her contact."""
+
+    __tablename__ = "fav_brands"
+
+    fav_brand_id = db.Column(db.Integer,
+                             nullable=False,
+                             primary_key=True)
+
+    relatp_id = db.Column(db.Integer,
+                          db.ForeignKey('relationships.relatp_id'))
+
+    fav_brand = db.Column(db.Text)
+
+    relatp = db.relationship("Relationship", backref=db.backref("fav_sports_teams"))
+
+
+class Other_Favorites(db.Model):
+    """Separately stores other favorites a user noted for his/her contact."""
+
+    __tablename__ = "other_favs"
+
+    other_favs_id = db.Column(db.Integer,
+                              nullable=False,
+                              primary_key=True)
+
+    relatp_id = db.Column(db.Integer,
+                          db.ForeignKey('relationships.relatp_id'))
+
+    other_fav = db.Column(db.Text)
+
+    relatp = db.relationship("Relationship", backref=db.backref("other_favs"))
+
+
+class Conversation_Log(db.Model):
+    """Separately stores each notable conversation a user had with his/her contact."""
+
+    __tablename__ = "convo_log"
+
+    convo_log_id = db.Column(db.Integer,
+                             nullable=False,
+                             primary_key=True)
+
+    relatp_id = db.Column(db.Integer,
+                          db.ForeignKey('relationships.relatp_id'))
+
+    convo = db.Column(db.Text)
+
+    relatp = db.relationship("Relationship", backref=db.backref("convo_log"))
+
+
+class Trait(db.Model):
+    """Separately stores each trait a user noted about his/her contact."""
+
+    __tablename__ = "traits"
+
+    trait_id = db.Column(db.Integer,
+                         nullable=False,
+                         primary_key=True)
+
+    relatp_id = db.Column(db.Integer,
+                          db.ForeignKey('relationships.relatp_id'))
+
+    trait = db.Column(db.Text)
+
+    relatp = db.relationship("Relationship", backref=db.backref("traits"))
+
+
+class Connection(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    provider_id = db.Column(db.String(255))
+    provider_user_id = db.Column(db.String(255))
+    access_token = db.Column(db.String(255))
+    secret = db.Column(db.String(255))
+    display_name = db.Column(db.String(255))
+    profile_url = db.Column(db.String(512))
+    image_url = db.Column(db.String(512))
+    rank = db.Column(db.Integer)
+
+
+
+
+
+def connect_to_db(app):
+    """Conenct the database to the Flask app."""
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///contacts'
+    db.app = app
+    db.init_app(app)
 
 if __name__ == "__main__":
     """This is useful for running this module interactively. This will leave me
--- server.py
+++ server.py
@@ -2,18 +2,55 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask, render_template, request, redirect, session
-from flask_debeugtoolbar import DebugToolbarExtension
+from flask import Flask, render_template, request, redirect, session, current_app, flash, url_for
+from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db, User
+from model import connect_to_db, db, Connection
 
+import os
+
+
+# from flask.ext.social import Social, SQLAlchemyConnectionDatastore, login_failed
+# from flask.ext.social.datastore import SQLAlchemyConnectionDatastore
+# from flask.ext.social.utils import get_provider_or_404, get_connection_values_from_oauth_response
+# from flask.ext.social.views import connect_handler
+# from flask.ext.sqlalchemy import SQLAlchemy
+
+# from flask.ext.security import LoginForm, current_user, login_required, login_user, Security, SQLAlchemyUserDatastore
+
+# from flask.ext.assets import Environment
+
+# Instantiates Flask.
 app = Flask(__name__)
 
 # Required to use Flask sessions and the debug DebugToolbarExtension
 app.secret_key = "ILoveStephenColbert"
 
+# app.config['SOCIAL_GOOGLE'] = 'client_secrets.sh'
+
+# app.config['SOCIAL_GOOGLE'] = {
+#     'consumer_key': os.environ.get('GOOGLE_OAUTH2_CLIENT_ID'),
+#     'consumer_secret': os.environ.get('GOOGLE_OAUTH2_CLIENT_SECRET')
+# }
+
+# security_ds = SQLAlchemyUserDatastore(db, models.User, models.Role)
+# social_ds = SQLAlchemyConnectionDatastore(db, models.Connection)
+
+# app.security = Security(app, security_ds)
+# app.social = Social(app, social_ds)
+
+# fsocial = Social(app, SQLAlchemyConnectionDatastore(db, Connection))
+
+# app.config['SECRET_KEY'] = 'your-secret-key'
+
+# REDIRECT_URI = '/import_contacts'
+
+# oauth2 = UserOAuth2(app)
+
 # Raises an error when an undefined variable is used in Jinja2.
 app.jinja_env.undefined = StrictUndefined
+
+# gd_client = gdata.contacts.client.ContactsClient(source='contacts')
 
 
 @app.route('/')
@@ -21,3 +58,140 @@ def index():
     """Homepage."""
 
     return render_template("homepage.html")
+
+
+@app.route('/login')
+def login():
+    """Page where users sign-in with oAuth from Google."""
+
+    # if current_user.is_authenticated():
+    #     return redirect(request.referrer or '/')
+
+    # return render_template('login.html', form=LoginForm())
+    return render_template('login_form.html')
+
+
+@app.route('/register')
+def register():
+    """Page where users registers for my app."""
+
+    return render_template('registration_form.html')
+# class SocialLoginError(Exception):
+#     def __init__(self, provider):
+#         self.provider = provider
+
+
+# @app.context_processor
+# def template_extras():
+#     return dict(
+#         google_analytics_id=app.config.get('GOOGLE_ANALYTICS_ID', None))
+
+
+# @login_failed.connect_via(app)
+# def on_login_failed(sender, provider, oauth_response):
+#     app.logger.debug('Social Login Failed via %s; '
+#                      '&oauth_response=%s' % (provider.name, oauth_response))
+
+#     # Save the oauth response in the session so we can make the connection
+#     # later after the user possibly registers
+#     session['failed_login_connection'] = \
+#         get_connection_values_from_oauth_response(provider, oauth_response)
+
+#     raise SocialLoginError(provider)
+
+
+# @app.errorhandler(SocialLoginError)
+# def social_login_error(error):
+#     return redirect(
+#         url_for('register', provider_id=error.provider.id, login_failed=1))
+
+
+# @app.route('/register', methods=['GET', 'POST'])
+# @app.route('/register/<provider_id>', methods=['GET', 'POST'])
+# def register(provider_id=None):
+#     if current_user.is_authenticated():
+#         return redirect(request.referrer or '/')
+
+#     form = RegisterForm()
+
+#     if provider_id:
+#         provider = get_provider_or_404(provider_id)
+#         connection_values = session.get('failed_login_connection', None)
+#     else:
+#         provider = None
+#         connection_values = None
+
+#     if form.validate_on_submit():
+#         ds = current_app.security.datastore
+#         user = ds.create_user(email=form.email.data, password=form.password.data)
+#         ds.commit()
+
+#         # See if there was an attempted social login prior to registering
+#         # and if so use the provider connect_handler to save a connection
+#         connection_values = session.pop('failed_login_connection', None)
+
+#         if connection_values:
+#             connection_values['user_id'] = user.id
+#             connect_handler(connection_values, provider)
+
+#         if login_user(user):
+#             ds.commit()
+#             flash('Account created successfully', 'info')
+#             return redirect(url_for('profile'))
+
+#         return render_template('thanks.html', user=user)
+
+#     login_failed = int(request.args.get('login_failed', 0))
+
+#     return render_template('register.html',
+#                            form=form,
+#                            provider=provider,
+#                            login_failed=login_failed,
+#                            connection_values=connection_values)
+
+
+@app.route('/landing_page')
+# @login_required
+def landing_page():
+    """Page where users land after logging in or signing up."""
+
+    return render_template('landing_page.html')
+    # return render_template('profile.html',
+    #                        google_conn=social.google.get_connection())
+
+
+# @app.route('/profile/<provider_id>/post', methods=['POST'])
+# @login_required
+# def social_post(provider_id):
+
+#     access_token = request.form.get('message', None)
+#     print access_token
+    # return redirect(url_for('profile'))
+
+
+
+# # html that app.route should be the outermost decorator.
+# @app.route('/needs_credentials')
+# @oauth2.required
+# def example():
+#     # http is authorized with the user's credentials and can be used
+#     # to make http calls.
+#     http = oauth2.http()
+
+# @app.route('/info')
+# @oauth2.required
+# def info():
+#     return "Hello, {} ({})".format(oauth2.email, oauth2.user_id)
+
+
+if __name__ == "__main__":
+    # Setting this to be true so that I can invoke the DebugToolbarExtension
+    app.debug = True
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    # fsocial.init_app(app)
+
+    # port 5000 required for vagrant
+    app.run(debug=True, host='0.0.0.0', port=5000)
